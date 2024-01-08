5e4b35fdbf2e5aaaed6822ddec95ccfde65a44d1
Artem <artemzhdanov1299@gmail.com>
2024-01-08 22:48:05 +0200

    ///

--- project_wpf/MainWindow.xaml.cs
+++ project_wpf/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace project_wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
--- project_wpf/Menu.xaml.cs
+++ project_wpf/Menu.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace project_wpf
 {
-    /// <summary>
-    /// Interaction logic for Menu.xaml
-    /// </summary>
     public partial class Menu : Window
     {
         public static MainWindow Window;
